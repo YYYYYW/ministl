0b8aba5d4ac01801ff41c9a759dee17dc91bae6c
YYYYYW <yajie_w2@163.com>
2022-07-05 22:58:01 +0800

    alloc_test and README

--- test/alloc_test.cpp
+++ test/alloc_test.cpp
@@ -22,15 +22,20 @@ struct alloc_test {
 };
 
 int main () {
-    std::vector<int, ministl::allocator<int>> intVec;
-    for (int i = 0; i < 10; ++i)
-        intVec.push_back(i);
-    for (int i = 0; i < 10; ++i)
-        cout << intVec[i] << endl;
+    // std::vector<int, ministl::allocator<int>> intVec;
+    // for (int i = 0; i < 10; ++i)
+    //     intVec.push_back(i);
+    // for (int i = 0; i < 10; ++i)
+    //     cout << intVec[i] << endl;
     std::vector<alloc_test, ministl::allocator<alloc_test>> testVec;
     alloc_test t1;
+    cout << "sizeof(alloc_test): " << sizeof(alloc_test) << endl;
     testVec.push_back(t1);
     testVec.push_back(alloc_test());
+    cout << testVec.size() << endl;
     testVec[0].show();
     testVec[1].show();
+    testVec.erase(testVec.begin());
+    cout << testVec.size() << endl;
+    testVec[0].show();
 }
